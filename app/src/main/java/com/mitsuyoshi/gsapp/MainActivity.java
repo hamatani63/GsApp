ca9dd88148c55b225d54436b5da94a050a7dd584
Mitsuyoshi Hamatani <hamatani63@gmail.com>
2016-04-16 01:24:07 +0900

    Add a settings.

--- app/src/main/java/com/mitsuyoshi/gsapp/MainActivity.java
+++ app/src/main/java/com/mitsuyoshi/gsapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,21 +14,20 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    @Override
-//    public boolean onCreateOptionMenu(Menu menu){
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionalItemSelected(MenuItem item){
-//        int id = item.getItemId();
-//
-//        if(id == R.id.action_settings){
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
